d502b6779a064de5d3bab4e7ea071acff4db88ef
GitHub Action <action@github.com>
2025-08-07 08:19:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/piochoi/odsiconcodesuggestion/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/piochoi/odsiconcodesuggestion/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.piochoi.odsiconcodesuggestion.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.piochoi.odsiconcodesuggestion.MyBundle
+import com.github.piochoi.odsiconcodesuggestion.services.MyProjectService
 import javax.swing.JButton
 
 
